7d3e83ada87de3f5463ae41f320d947d687b0b54
mjkushman <mjkushman@gmail.com>
2024-11-08 14:38:55 -0800

    Created new-agent route. Reorg'd routes behind auth

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -11,11 +11,11 @@ import { User } from "@/types";
 import api from "@/utils/api";
 import { RequireAuth } from "./layouts/RequireAuth";
 import { AccountRoot } from "./routes/account";
+import { NewAgent } from "./routes/account/new-agent";
 
 const createAppRouter = (user: User | null) => {
   return createBrowserRouter([
     // each route goes here
-
     {
       path: "/",
       element: <MainLayout />,
@@ -29,19 +29,25 @@ const createAppRouter = (user: User | null) => {
         },
         {
           path: "account",
-          element: (
-            <RequireAuth>
-              <AccountRoot />
-            </RequireAuth>
-          ),
-          // lazy: async () => {
-          //   const { AccountRoot } = await import("./routes/account");
-          //   return { Component: AccountRoot };
-          // },
-          loader: async () => {
-            if (!user || !user.accountId) return null;
-            return api.get(`accounts/`); // passes accountId as part of token
-          },
+          element: <RequireAuth />,
+          children: [
+            {
+              path: "",
+              element: <AccountRoot />,
+              loader: async () => {
+                if (!user || !user.accountId) return null;
+                return api.get(`accounts/`); // passes accountId as part of token
+              },
+            },
+            {
+              path: "new-agent",
+              element: <NewAgent />,
+              loader: async () => {
+                if (!user || !user.accountId) return null;
+                return api.get(`accounts/`); // passes accountId as part of token
+              },
+            },
+          ],
         },
         {
           path: "docs",
@@ -56,13 +62,6 @@ const createAppRouter = (user: User | null) => {
         {
           path: "",
           element: <Navigate to="/auth/login" replace />, // redirects home
-        },
-        {
-          path: "register",
-          lazy: async () => {
-            const { RegisterRoute } = await import("./routes/auth/register");
-            return { Component: RegisterRoute };
-          },
         },
         {
           path: "login",
