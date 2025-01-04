1a058f86d1e798086ad21c63d3e3dd3cb7107c7e
mjkushman <mjkushman@gmail.com>
2025-01-04 15:11:08 -0800

    base api url

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,6 +1,6 @@
 import { localStorageKey} from "@/utils/authService";
 
-export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3001/";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const api = {
   get: async <T,>(url: string): Promise<T> => {
